b644a1294810c425dbce62151ece49bc2252a08e
xps-admin <strategicmindsadvisory@gmail.com>
2026-07-14 01:25:09 -0400

    feat: xtreme-scraper

---
+++ scraper/__init__.py
@@ -0,0 +1,3 @@
+from .core import scrape, XtremeScraper, ScrapeConfig
+from .swarm import XtremeSwarm, run_swarm, SwarmTask, SwarmAgent
+__all__ = ['scrape', 'XtremeScraper', 'ScrapeConfig', 'XtremeSwarm', 'run_swarm']
